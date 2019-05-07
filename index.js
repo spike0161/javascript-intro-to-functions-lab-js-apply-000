da9d51a8f12be3f6671eb686a5d6722bdeeef793
Dean Hildebrand <dean.hildebrand88@gmail.com>
2019-05-07 23:19:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,14 +17,15 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   if (string.toLowerCase() === string) {
-    return "I can't hear you"
+    return ("I can't hear you")
   }
   else if (string === "I love you, Grandma.") {
-    return "I love you, too."
+    return ("I love you, too.")
   }
   else {
-    return "I can\'t hear you"
+    return ("I can\'t hear you")
   }
+}
  
 
 
