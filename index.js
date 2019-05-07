03bc07362d915a7557b97e1db3b1c1225847dcfe
Dean Hildebrand <dean.hildebrand88@gmail.com>
2019-05-07 23:06:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,28 +15,20 @@ function logWhisper(string) {
 }
 
 
-var uppercase = "HELLO!"
- 
-uppercase.toUpperCase() === uppercase // true
- 
-var lowercase = 'hello!'
- 
-lowercase.toLowerCase() === lowercase // true
- 
-var mixedCase = 'Hi there!'
- 
-mixedCase.toLowerCase() === mixedCase // false
- 
-mixedCase.toUpperCase() === mixedCase // false
+describe('sayHiToGrandma(string)', function() {
+  it('returns "I can\'t hear you!" if `string` is lowercase', function() {
+    expect(sayHiToGrandma('hello')).toEqual("I can't hear you!")
+  })
 
-function sayHiToGrandma(string) {
-  return ("I can\'t hear you!").toLowerCase()
-}
+  it('returns "YES INDEED!" if `string` is uppercase', function() {
+    expect(sayHiToGrandma('HELLO')).toEqual("YES INDEED!")
+  })
 
-function sayHiToGrandma(string) {
-     
-  
-}
+  it('returns "I love you, too." if `string` is "I love you, Grandma."`', function() {
+    expect(sayHiToGrandma("I love you, Grandma.")).toEqual("I love you, too.")
+  })
+})
+
 
 
 
