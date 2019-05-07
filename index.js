909d936718c8509ede99c12ec31e91d59c2305e5
Dean Hildebrand <dean.hildebrand88@gmail.com>
2019-05-07 22:52:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -39,12 +39,12 @@ function sayHiToGrandma(string) {
   
 }
 
-function sayHiToGrandma(string) {
-  if(string === "I love you Grandma"){
-    return("I love you too")
-  }
+// function sayHiToGrandma(string) {
+//   if(string === "I love you Grandma"){
+//     return("I love you too")
+//   }
   
-}
+// }
 
 
 
