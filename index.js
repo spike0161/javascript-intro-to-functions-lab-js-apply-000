1ba42a5f749cc6de4516975cd08cfcd00bcba2b7
Dean Hildebrand <dean.hildebrand88@gmail.com>
2019-05-07 23:00:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,9 +15,22 @@ function logWhisper(string) {
 }
 
 
+var uppercase = "HELLO!"
+ 
+uppercase.toUpperCase() === uppercase // true
+ 
+var lowercase = 'hello!'
+ 
+lowercase.toLowerCase() === lowercase // true
+ 
+var mixedCase = 'Hi there!'
+ 
+mixedCase.toLowerCase() === mixedCase // false
+ 
+mixedCase.toUpperCase() === mixedCase // false
 
 function sayHiToGrandma(string) {
-  return 
+  return string.toLowerCase()
 }
 
 function sayHiToGrandma(string) {
